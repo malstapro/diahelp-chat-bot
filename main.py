ea204a05ad3f1fe82db08b02692f278abc2d2a47
tesla33IO <52202562+tesla33IO@users.noreply.github.com>
2020-11-10 17:48:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,45 +6,38 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
+
+    btns = [
+        [types.KeyboardButton(text="Помощь")],
+        [types.KeyboardButton(text="Настройки")],
+        [types.KeyboardButton(text="Информация")]
+    ]
+
+    kb = types.ReplyKeyboardMarkup(keyboard=btns)
+
     await message.answer("Здравствуйте!\n"
                          "Я бот помощник для людей болеющих сахарным диабетом\n"
                          "Я могу предоставить вам разнообразную полезную информацию"
-                         "\nЧтобы узнать подробней, используйте команду - /help"
-                         "\n\n*САМОЛЕЧЕНИЕ МОЖЕТ НАВРЕДИТЬ ВАШЕМУ ЗДОРОВЬЮ!*", parse_mode="Markdown")
-@dp.message_handler(commands=['help'])
+                         "\nЧтобы узнать подробней, нажмите на кнопку *Помощь*."
+                         "\n\n*САМОЛЕЧЕНИЕ МОЖЕТ НАВРЕДИТЬ ВАШЕМУ ЗДОРОВЬЮ!*", parse_mode="Markdown", reply_markup=kb)
+
+@dp.message_handler(content_types=['text'])
 async def send_help(message: types.Message):
+    if message.text == "Помощь":
+        btns = [[types.inline_keyboard.InlineKeyboardButton("Таблица ХЕ",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Термины",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Ситуации",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Симптомы",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Контроль сахара",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Питание",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Инсулин",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Физ. нагрузка",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Повседневная жизнь",callback_data='test')],
+        [types.inline_keyboard.InlineKeyboardButton("Графики",callback_data='test')]]
+        kb = types.inline_keyboard.InlineKeyboardMarkup(inline_keyboard=btns)
 
-    btns = [[types.inline_keyboard.InlineKeyboardButton("Помощь",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Таблица ХЕ",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Термины",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Ситуации",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Симптомы",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Контроль сахара",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Питание",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Инсулин",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Физ. нагрузка",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Повседневная жизнь",callback_data='test')],
-    [types.inline_keyboard.InlineKeyboardButton("Графики",callback_data='test')]]
-    kb = types.inline_keyboard.InlineKeyboardMarkup(inline_keyboard=btns)
+        await message.answer("КОМАНДЫ", reply_markup=kb)
 
-    await message.answer("КОМАНДЫ", reply_markup=kb)
-
-    # await message.answer("*ПОМОЩЬ*\n"
-    #                      "_Команды_:\n"
-    #                      "/help\n"
-    #                      "/hetable\n"
-    #                      "/termin\n"
-    #                      "/situation\n"
-    #                      "/simptom\n"
-    #                      "/suger\n"
-    #                      "/food\_advice\n"
-    #                      "/insulin\n"
-    #                      "/exercises\n"
-    #                      "/everyday\n"
-    #                      "/get\_graph\n\n"
-    #                      "Для более детальной информации про команду напишите"
-    #                      "\n`/help [команда]`, где вместо `[команда]` вставьте интересующую вас команду",
-    #                      parse_mode="Markdown")
 
 
 
