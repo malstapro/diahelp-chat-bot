f696294cfb4cc839fd9d5521675f27be20621d2e
tesla33IO <52202562+tesla33IO@users.noreply.github.com>
2021-01-03 14:15:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 TOKEN = '1478785019:AAHb8n7Df4_sa_-HXShFM9QwbQepkt6CcN4'
-MONGO_TOKEN = 'mongodb+srv://admintester:verystrongpassword@cluster0.8jafe.mongodb.net/test?retryWrites=true&w=majority'
+MONGO_TOKEN = 'mongodb+srv://Admin:isverystrongpassword@cluster0.nyjni.mongodb.net/users?retryWrites=true&w=majority'
