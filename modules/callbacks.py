7f4d7c0e4a9cb35033cbb70bb8c6fa30271fcdc9
tesla33IO <startyom14@gmail.com>
2022-01-28 13:54:56 +0200

    Chart functionality added

--- modules/callbacks.py
+++ modules/callbacks.py
@@ -1,4 +1,7 @@
 import asyncio
+import os
+import io
+from PIL import Image
 from datetime import datetime, timedelta
 import json
 
@@ -16,7 +19,7 @@ from modules import keyboards as kb
 from modules import messages
 from modules.states import Sugar, Settings, Rating, Convert, FoodSearch
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 bot = Bot(token=credentials.TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -33,12 +36,13 @@ defaultSugar = {
 async def finish_state(state: FSMContext):
     try:
         async with state.proxy() as data:
-            tmp = data['add-sugar-last-time']
+            tmp = [data['add-sugar-last-time'], data['chart-last-time']]
             await state.finish()
         async with state.proxy() as data:
-            data['add-sugar-last-time'] = tmp
+            data['add-sugar-last-time'] = tmp[0]
+            data['chart-last-time'] = tmp[1]
     except Exception as e:
-        if e.__class__.__name__ + ': ' + str(e) == 'KeyError: \'add-sugar-last-time\'':
+        if e.__class__.__name__ == 'KeyError':
             await state.finish()
         else:
             logger.error(e.__class__.__name__ + ': ' + str(e))
@@ -99,19 +103,53 @@ async def back_to_menu(m: types.Message):
 
 
 @dp.message_handler(text='📊 Статистика')
-async def statistics(m: types.Message):
+async def statistics(m: types.Message, state: FSMContext):
     try:
-        await bot.send_message(m.from_user.id,
-                               'Функція виводу статистики зараз не працює. Якщо у вас виникли питання зверниться до розробника @tesla33io')
-        # sugars = sugar[m.from_user.id]['sugars'][f'{datetime.now().year}'][f'{datetime.now().month}'][f'{datetime.now().day}']
-        # time_list = []
-        # index_list = []
-        #
-        # for time in sugars:
-        #     time_list.append(int(str(time).replace('-', '')))
-        #     index_list.append(float(str(sugars[time])))
-        # plt.bar(time_list, index_list)
-        # plt.show()
+        async with state.proxy() as data:
+            try:
+                if (timedelta(hours=5) - (
+                            datetime.now(tz=pytz.timezone("Europe/Kiev")) - data['chart-last-time'])) <= timedelta(
+                            minutes=0, seconds=0):
+                    sugars = sugar[m.from_user.id]['sugars'][f'{datetime.now().year}'][f'{datetime.now().month}'][f'{datetime.now().day}']
+                    time_list = []
+                    index_list = []
+
+                    for item in sugars:
+                        time_list.append(str(item).replace('-', ':'))
+                        index_list.append(float(str(sugars[item])))
+                    with io.BytesIO() as buf:
+                        buf.flush()
+                        plt.plot(time_list, index_list)
+                        plt.savefig(buf, format='png')
+                        plt.clf()
+                        buf.seek(0)
+                        await bot.send_photo(m.chat.id, buf, caption=f"{datetime.now().date()}")
+                        buf.close()
+                    data['chart-last-time'] = datetime.now(tz=pytz.timezone("Europe/Kiev"))
+                else:
+                    wait_time = timedelta(hours=5) - (
+                                datetime.now(tz=pytz.timezone("Europe/Kiev")) - data['chart-last-time'])
+                    print(wait_time)
+                    await bot.send_message(m.from_user.id, messages.waite_add_sugar.format(
+                        f'{str(wait_time).split(":")[0]}:{str(wait_time).split(":")[1]}:{str(wait_time).split(":")[2].split(".")[0]}',
+                        'години' if wait_time > timedelta(hours=1) else 'годину' if wait_time == timedelta(
+                            hours=1) else 'хвилин' if wait_time > timedelta(minutes=4) else "хвилини"), parse_mode=ParseMode.MARKDOWN)
+            except KeyError:
+                sugars = sugar[m.from_user.id]['sugars'][f'{datetime.now().year}'][f'{datetime.now().month}'][f'{datetime.now().day}']
+                time_list = []
+                index_list = []
+                for item in sugars:
+                    time_list.append(str(item).replace('-', ':'))
+                    index_list.append(float(str(sugars[item])))
+                with io.BytesIO() as buf:
+                    buf.flush()
+                    plt.plot(time_list, index_list)
+                    plt.savefig(buf, format='png')
+                    plt.clf()
+                    buf.seek(0)
+                    await bot.send_photo(m.chat.id, buf, caption=f"{datetime.now().date()}")
+                    buf.close()
+                data['chart-last-time'] = datetime.now(tz=pytz.timezone("Europe/Kiev"))
     except Exception as e:
         logger.error(e.__class__.__name__ + ': ' + str(e))
 
